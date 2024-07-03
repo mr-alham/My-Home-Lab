a972ace7fd92fd46a297056b8c85d97887a42fc8
Alham <alhamazhar4@gmail.com>
2024-07-04 00:34:40 +0530

    install.sh: updated

--- Zsh/install.sh
+++ Zsh/install.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.zsh/powerlevel10k
-  echo 'source ~/.zsh/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+echo 'source ~/.zsh/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 
-  if [[ ! -d ~/.zsh/plugins ]]; then
-      mkdir -p ~/.zsh/plugins/zsh-syntax-highlighting
-      mkdir -p ~/.zsh/plugins/zsh-autosuggestions
-      mkdir -p ~/.zsh/plugins/zsh-history-substring-search
-      elif [[ ! -d ~/.zsh/plugins/zsh-syntax-highlighting ]]; then
-          mkdir -p ~/.zsh/plugins/zsh-syntax-highlighting
-      fi
-      elif [[ ! -d ~/.zsh/plugins/zsh-autosuggestions ]]; then
-          mkdir -p ~/.zsh/plugins/zsh-autosuggestions
-      fi
-      elif [[ ! -d ~/.zsh/plugins/zsh-history-substring-search ]]; then
-          mkdir -p ~/.zsh/plugins/zsh-history-substring-search
-      fi
-  fi
+if [[ ! -d ~/.zsh/plugins ]]; then
+    mkdir -p ~/.zsh/plugins/zsh-syntax-highlighting
+    mkdir -p ~/.zsh/plugins/zsh-autosuggestions
+    mkdir -p ~/.zsh/plugins/zsh-history-substring-search
+    elif [[ ! -d ~/.zsh/plugins/zsh-syntax-highlighting ]]; then
+        mkdir -p ~/.zsh/plugins/zsh-syntax-highlighting
+    fi
+    elif [[ ! -d ~/.zsh/plugins/zsh-autosuggestions ]]; then
+        mkdir -p ~/.zsh/plugins/zsh-autosuggestions
+    fi
+    elif [[ ! -d ~/.zsh/plugins/zsh-history-substring-search ]]; then
+        mkdir -p ~/.zsh/plugins/zsh-history-substring-search
+    fi
+fi
 
-  # Syntax-highlighting
-  curl -Lo ~/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh https://raw.githubusercontent.com/zsh-users/zsh-syntax-highlighting/master/zsh-syntax-highlighting.zsh
-  
-  echo "check this file, if same as uncommented one invert comments
-  #curl -Lo ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh https://raw.githubusercontent.com/zsh-users/zsh-syntax-highlighting/master/zsh-syntax-highlighting.plugin.zsh
-  curl -Lo ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh https://raw.githubusercontent.com/mr-alham/My-Home-Lab/main/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+# Syntax-highlighting
+curl -Lo ~/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh https://raw.githubusercontent.com/zsh-users/zsh-syntax-highlighting/master/zsh-syntax-highlighting.zsh
 
-  # Auto-suggestions
-  curl -Lo ~/.zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh https://raw.githubusercontent.com/zsh-users/zsh-autosuggestions/master/zsh-autosuggestions.zsh
-  curl -Lo ~/.zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh https://raw.githubusercontent.com/zsh-users/zsh-autosuggestions/master/zsh-autosuggestions.plugin.zsh
+echo "check this file, if same as uncommented one invert comments
+#curl -Lo ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh https://raw.githubusercontent.com/zsh-users/zsh-syntax-highlighting/master/zsh-syntax-highlighting.plugin.zsh
+curl -Lo ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh https://raw.githubusercontent.com/mr-alham/My-Home-Lab/main/Zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
 
-  # history-substring-search
-  curl -Lo ~/.zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh https://raw.githubusercontent.com/zsh-users/zsh-history-substring-search/master/zsh-history-substring-search.zsh
+# Auto-suggestions
+curl -Lo ~/.zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh https://raw.githubusercontent.com/zsh-users/zsh-autosuggestions/master/zsh-autosuggestions.zsh
+curl -Lo ~/.zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh https://raw.githubusercontent.com/zsh-users/zsh-autosuggestions/master/zsh-autosuggestions.plugin.zsh
 
-  curl -Lo ~/.zsh/custom-zsh-config https://raw.githubusercontent.com/mr-alham/My-Home-Lab/main/custom-zsh-config
+# history-substring-search
+curl -Lo ~/.zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh https://raw.githubusercontent.com/zsh-users/zsh-history-substring-search/master/zsh-history-substring-search.zsh
 
-  curl -Lo ~/.zsh/p10k.zsh https://raw.githubusercontent.com/mr-alham/My-Home-Lab/main/p10k.zsh
+curl -Lo ~/.zsh/custom-zsh-config https://raw.githubusercontent.com/mr-alham/My-Home-Lab/main/Zsh/custom-zsh-config
+curl -Lo ~/.zsh/p10k.zsh https://raw.githubusercontent.com/mr-alham/My-Home-Lab/main/Zsh/p10k.zsh
+
+curl -Lo ~/.zshrc https://raw.githubusercontent.com/mr-alham/My-Home-Lab/main/Zsh/zshrc
